edaa8e033357e4f0e94f202e83b8b8a4d404f8ea
Pat Howell <patrick.howell@outlook.com>
2016-04-01 22:13:03 +0100

    Added test web project for api play around

---
+++ WebAppTets/Scripts/Angular/app.js
@@ -0,0 +1,13 @@
+﻿var testModule = angular.module('TestApp', []);
+
+testModule.controller('TestController', function () {
+    var self = this;
+
+    self.animals = [
+        { name: "Platypus" },
+        { name: "Lion" },
+        { name: "Hippo" },
+        { name: "Cappybara" },
+        { name: "Penguin" }
+    ]
+});
---
+++ WebAppTets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppTets.Startup))]
+namespace WebAppTets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
