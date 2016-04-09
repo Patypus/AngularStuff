767f90568a5994d7b496830ec178883813c43645
Pat Howell <patrick.howell@outlook.com>
2016-04-09 22:09:31 +0100

    Changes

--- DriverDetails/simpleApp.js
+++ DriverDetails/simpleApp.js
@@ -4,16 +4,40 @@ simpleModule.factory('factory', function () {
 });
 
 simpleModule.service('MainService', function () {
-});
+    var service = this;
 
-simpleModule.controller('MainController', function () {
-    var self = this;
+    var conservationStatus = [
+        { name: "Extinct", abbreviation: "EX" },
+        { name: "Extinct in the Wild", abbreviation: "EW" },
+        { name: "Critically Endangered", abbreviation: "CR" },
+        { name: "Endangered", abbreviation: "EN" },
+        { name: "Vulnerable", abbreviation: "VU" },
+        { name: "Near Threatened", abbreviation: "NT" },
+        { name: "Conservation Dependent", abbreviation: "CD" },
+        { name: "Least Conern", abbreviation: "LC" }
+    ];
 
-    self.animals = [
+    var animals = [
         { name: "Platypus" },
         { name: "Lion" },
         { name: "Hippo" },
         { name: "Cappybara" },
         { name: "Penguin" }
     ]
+
+    service.GetConservationStatuses = function () {
+        return conservationStatus;
+    }
+
+    service.GetAnimals = function () {
+        return animals;
+    }
+});
+
+simpleModule.controller('MainController', function (MainService) {
+    var self = this;
+
+    self.animals = function () {
+        return MainService.GetAnimals();
+    }
 });
